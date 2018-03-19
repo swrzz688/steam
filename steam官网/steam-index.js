31143b6f37a8e5aed50c57325ffb5b05afd6519d
swrzz688 <1209448537qq.com>
2018-03-19 15:55:00 +0800

    steam数据请求3.19

--- steam官网/steam-index.js
+++ steam官网/steam-index.js
@@ -1,23 +1,41 @@
-//详情页cookie
-
-
-
-
-
 
 //轮播图一jsonp跨域请求数据
 
-var yuanban = document.getElementsByClassName("hezi")[0]  //原版
-var dahezi = document.getElementsByClassName("dahezi")[0]
+var yuanban = document.getElementsByClassName("hezi")[0]  	//原版
+var dahezi = document.getElementsByClassName("dahezi")[0]	//大盒子
 //声明回调函数 回调函数名要与callback一致
+//
+window.onload = function(){
+	var scr = document.createElement("script")
+	scr.setAttribute("src","http://ie19852360.51mypc.cn?callback=callbackfn")
+	document.getElementsByTagName("head")[0].appendChild(scr)
+}
+
 function callbackfn(data){
 	for(var i in data){
-		newban = yuanban.cloneNode(true)
-		dahezi.appendChild(newban)
-		var youimg  = data[i].imgUrl //右边的所有图片
-		var xiaotu = newban.querySelectorAll(".youtu a img")
-		var zuotu = newban.querySelectorAll(".zuotu a img")
-		var tanchucen = newban.querySelectorAll(".tanchucen .img img")
+		newban = yuanban.cloneNode(true) //克隆出新盒子
+		dahezi.appendChild(newban)		 //插入到大盒子
+		var youimg  = data[i].imgUrl     //所有图片
+		var xiaotu = newban.querySelectorAll(".youtu a img") //右边四张小图
+		var xiaotua = newban.querySelectorAll(".youtu a")    //右边所有a标签
+		var zuotu = newban.querySelectorAll(".zuotu a img")  //中间的四张图片
+		var tanchucen = newban.querySelectorAll(".tanchucen .img img") //弹出层图片
+		var gameId = data[i].gameId   //ID值
+		var url = data[i].url
+		for(var a = 0;a < youimg.length;a++){
+			// var id= newban.setAttribute("gameId",gameId)
+			// console.log(id)
+			//右边小图
+			xiaotu[a].setAttribute("src",data[i].imgUrl[a])
+			//中间图片
+			zuotu[a].setAttribute("src",youimg[a])
+			//弹出层图片 
+			tanchucen[a].setAttribute("src",youimg[a])
+			xiaotua[a].setAttribute("gameIdzhi",gameId)
+			xiaotua[a].setAttribute("href","javascript:void(0)")
+			//右边小图链接地址
+			// newban.querySelectorAll(".xiaotu a")[a].setAttribute("href",data[i].url)
+		}
 		//弹出层日期
 		var getdata=data[i].date
 		var newtime = getdata.split("-")
@@ -25,6 +43,10 @@ function callbackfn(data){
 		//弹出层和右边游戏名
 		newban.querySelectorAll(".gameName")[0].innerHTML = data[i].name
 		newban.querySelectorAll(".ming")[0].innerHTML = data[i].name
+		//中间第一张图片默认显示
+		zuotu[0].style.display = "block"
+		//弹出层第一张图片默认显示
+		tanchucen[0].style.display = "block"
 		//获取折扣价格
 		var zhekou = data[i].discount
 		var many = data[i].originPrice
@@ -80,16 +102,6 @@ function callbackfn(data){
 			newban.querySelectorAll(".pinse")[0].innerHTML = "无评论"
 		}
 
-		for(var a = 0;a < youimg.length;a++){
-			//右边小图
-			xiaotu[a].setAttribute("src",data[i].imgUrl[a])
-			//中间图片
-			zuotu[a].setAttribute("src",youimg[a])
-			//弹出层图片 
-			tanchucen[a].setAttribute("src",youimg[a])
-			//右边小图链接地址
-			newban.querySelectorAll(".xiaotu a")[a].setAttribute("href",data[i].url)
-		}
 		//版本图标
 		var banben = data[i].platform
 		var win = newban.querySelectorAll(".tubiao .win")[0]
@@ -116,8 +128,10 @@ function callbackfn(data){
 		label.parentNode.removeChild(label) 
 		
 		
-	}
+	}	
 	dahezi.removeChild(yuanban)
+	//第一个盒子默认显示
+	$(".dahezi .hezi").eq(0).addClass("show")
 	
 	// 自动生成小圆点
 	//存在执行顺序问题，所以点的生成应在初始化索引值之前
@@ -147,7 +161,6 @@ function callbackfn(data){
 	//隐藏图片和清除点的样式，让指向的当前图片显示和点添加样式
 	function lunboluoji(index,obj){
 		var $dotList = $(obj).closest(".aoto").find('.dian').find("span");
-		console.log($dotList)
 		// 隐藏所有的图片
 		$boxList.hide();
 		$boxList.removeClass("show");
@@ -194,28 +207,26 @@ function callbackfn(data){
 	$(".aoto .hezi").mouseenter(function(){
 		var $xiaotulist=$(this).find(".xiaotu a")
 		// 找到右边的小图
-		var $datulist= $(this).find(".zuotu img")	
+		var $datulist= $(this).find(".zuotu a img")	
 		//找到中间的背景图
 		for(a=0;a<$xiaotulist.length;a++){
 			$xiaotulist.eq(a).attr("index",a)
 			//设置非法属性
 		}
 		$xiaotulist.mouseenter(function(){
-			$(".hezi").find(".zuotu").css({
-				background:"none", 	
-				//鼠标移上时隐藏中间的背景图片
-			})
+			for(var a=0; a<$datulist.length;a++){
+				//动画队列问题，JQuery内部提供了停止动画队列的方法 “ stop() ”
+				//问题描述：不停地快速划过左边小图并且停止时，之前的动画尚未执行完毕，隐藏无效，图片无法回到回到正确位置
+		 		$datulist.eq(a).stop()
+			}
 			var zhi = $(this).attr("index")
 			$datulist.eq(zhi).fadeIn()  
 			//显示大图
 		})
 		$xiaotulist.mouseleave(function(){
-			$(".hezi").find(".zuotu").css({
-				background:"",		
-				//鼠标离开时显示中间的背景图片
-			})
 			var zhi = $(this).attr("index")
-			$datulist.eq(zhi).hide()	
+			$datulist.eq(zhi).hide()
+			$datulist.eq(0).fadeIn()	
 			//隐藏大图
 		})
 	})
@@ -250,14 +261,83 @@ function callbackfn(data){
 			auto();
 		},3000)
 	})
+
+
+
+	//详情页cookie
+	var gameIdarr =[] //声明一个数组，存放数据
+    //获取cookie
+
+	if(getCookie("gameidzhi")){
+		gameIdarr = getCookie("gameidzhi").split(",")
+	}
+
+	var select = ""
+	$(".xiaotu a").click(function(){
+
+		for(var j=0;j<gameIdarr.length;j++){
+ 			if($(this).attr("gameidzhi") == gameIdarr[j]){
+ 				gameIdarr.splice(j,1)
+ 			}
+ 		}
+ 		gameIdarr.push($(this).attr("gameIdzhi"))
+ 		var objid = {}  // 此对象写入的cookie数据
+
+		objid.gameidzhi = gameIdarr.toString() //将数组转换为字符串 再将字符串转换为对象，以便于写入cookie 
+
+		setCookie(objid,7) //设置cookie
+ 		location.href=$(this).attr("src")
+	})	
+
 }
 
 
-window.onload = function(){
-	var scr = document.createElement("script")
-	scr.setAttribute("src","http://ie19852360.51mypc.cn?callback=callbackfn")
-	document.getElementsByTagName("head")[0].appendChild(scr)
+var scrtwo = document.createElement("script")
+scrtwo.setAttribute("src","http://ie19852360.51mypc.cn?callback=callbackcookie&gameId="+getCookie("gameidzhi"))
+document.getElementsByTagName("head")[0].appendChild(scrtwo)
+
+
+function callbackcookie(data){
+	console.log(data)
+	for(var i in data){
+		var newchakan = $("<a></a>")
+		newchakan.html(data[i].name)
+		newchakan.attr("href",data[i].url)
+		$(".chakan").prepend(newchakan)
+	}
 }
+
+// 设置cookie函数
+function setCookie(data,time){
+	var d = new Date();
+	d.setDate(d.getDate() + time)
+	for(var i in data){
+	document.cookie = i + "=" + data[i] + ";expires=" + d;
+	}
+}
+
+// 获取cookie函数
+function getCookie(qishi){ 
+	var str = document.cookie;
+	// var qishi ="name"
+	var staterindex = str.indexOf(qishi)
+	var endindex = str.indexOf(";",staterindex)
+	if(endindex == -1){
+	endindex = str.length;
+	}
+	var result = str.slice(staterindex,endindex).split("=")[1]
+	return result
+}
+
+
+
+
+
+
+
+
+
+
 
 
 //侧边栏的图像
